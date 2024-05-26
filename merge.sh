2054235cf953e72d0630fbb845514c54155e4f7d
Knott Tellings <apprentice57@gmail.com>
2024-05-26 09:42:11 -0400

    Added Question 25. Renamed question texts from M# to R# since not all are being hosted by Matt going forward. Moved bonus question to separate folder. Now separating out Old and Reboot questions.

--- merge.sh
+++ merge.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-# Initialize an empty variable to store concatenated text
+#Merge Old Question Texts
 concatenated_text=""
-
-# Loop through each .txt file in the current directory
+# Merge question wording together with added question prefix
 for file in individualtxtfiles/*.txt; do
-    # Extract the first three characters of the filename
-    prefix=$(basename "$file" | cut -c 1-3)
-    # Add "Question XXX" followed by a newline to the concatenated text
+    firstchar=$(basename "$file" | cut -c 1-2)
+
+    if [[ ${firstchar} != "R" ]];then
+        prefix=$(basename "$file" | cut -c 1-4)
+        concatenated_text+="Question #$prefix\n"
+        concatenated_text+=$(cat "$file")
+        concatenated_text+="\n\n"
+    fi
+done
+
+rm OldQuestions.txt
+echo "$concatenated_text" > OldQuestions.txt
+
+#Merge New Question Texts
+concatenated_text=""
+for file in individualtxtfiles/R*.txt; do
+    prefix=$(basename "$file" | cut -c 2-4)
     concatenated_text+="Question #$prefix\n"
-    # Append the contents of the file to the concatenated text
     concatenated_text+=$(cat "$file")
-    # Add an extra newline for separation between files
+    concatenated_text+="\n\n"
+done
+
+rm RebootQuestions.txt
+echo "$concatenated_text" > RebootQuestions.txt
+
+#Merge All Question Texts
+concatenated_text=""
+# Merge question wording together with added question prefix
+for file in individualtxtfiles/*.txt; do
+    prefix=$(basename "$file" | cut -c 1-4)
+    concatenated_text+="Question #$prefix\n"
+    concatenated_text+=$(cat "$file")
     concatenated_text+="\n\n"
 done
 
 rm AllQuestions.txt
-
-# Print the concatenated text
 echo "$concatenated_text" > AllQuestions.txt
-
 
